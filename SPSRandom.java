122700b32f6da248567414bcdc6874c341c9ba12
JinTham <yehjintham@gmail.com>
2023-01-24 15:00:32 +0800

    commit

--- SPSRandom.java
+++ SPSRandom.java
@@ -3,14 +3,12 @@ import java.util.Random;
 public class SPSRandom {
     public Integer generateComputerChoice(){
         Random rand = new Random();
-        return rand.nextInt(3);
+        return rand.nextInt(3)+1;
     }
     public String checkWinner(Integer playerChoice, Integer computerChoice){
-        System.out.printf("Player %d vs Comp %d\n",playerChoice,computerChoice);
-        System.out.printf("Result: %d\n",(playerChoice-computerChoice)%3);
-        if ((playerChoice-computerChoice)%3==1){
+        if (((playerChoice-computerChoice)%3+3)%3==1){
             return "Player wins";
-        } else if ((playerChoice-computerChoice)%3==2){
+        } else if (((playerChoice-computerChoice)%3+3)%3==2){
             return "Computer wins";
         } else {
             return "It's a tie";
--- ScissorPaperStone.java
+++ ScissorPaperStone.java
@@ -1,6 +1,5 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 import java.util.Scanner;
 
 public class ScissorPaperStone {
@@ -8,6 +7,11 @@ public class ScissorPaperStone {
         SPSRandom comp = new SPSRandom();
         Scanner scanner = new Scanner(System.in);
         Integer playerChoice = 4;
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1, "Stone");
+        map.put(2,"Paper");
+        map.put(3,"Scissors");
+        Integer round = 1;
         while (playerChoice != 0){
             System.out.println("Enter (1)Stone, (2)Paper or (3)Scissors (0)Quit game: ");
             Integer compChoice = comp.generateComputerChoice();
@@ -15,8 +19,11 @@ public class ScissorPaperStone {
             if (playerChoice<=1 && playerChoice>=3){
                 System.out.println("Please enter valid number: ");
             } else {
+                System.out.printf("Round %d: Player (%s) vs Comp (%s)\n",round,map.get(playerChoice),map.get(compChoice));
                 System.out.println(comp.checkWinner(playerChoice, compChoice));
             }
+            round ++;
         }
+        scanner.close();
     }
 }
